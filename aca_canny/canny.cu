3b66c8f8bb5075f3d36adf878b695718912055e4
Eduardo Sousa <eduardosousa@ua.pt>
2015-01-12 23:52:53 +0000

    Shared memory ex1 - fix 001

--- aca_canny/canny.cu
+++ aca_canny/canny.cu
@@ -279,7 +279,7 @@ void cannyHost( const int *h_idata, const int w, const int h,
 __device__ pixel_t* getSubPixelMatrix(const pixel_t *in, const int x, const int y)
 {
     pixel_t subMatrix[9];
-    ĩnt counter = 0;
+    int counter = 0;
 
     for(int j = -const_khalf; j <= const_khalf; j++)
         for(int i = -const_khalf; i <= const_khalf; i++)
