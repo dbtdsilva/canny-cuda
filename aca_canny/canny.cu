18243239ddd9fb99bc07eb617bad104f014f2cab
Eduardo Sousa <eduardosousa@ua.pt>
2015-01-12 23:58:11 +0000

    Shared memory ex1 - fix 002

--- aca_canny/canny.cu
+++ aca_canny/canny.cu
@@ -295,7 +295,7 @@ __global__  void convolution_kernel(const pixel_t *in, const float *kernel, pixe
 
     if((x < (const_nx - const_khalf)) && (y < (const_ny - const_khalf)))
     {
-        int size = (blockDim.x + 2*const_khalf)*(blockDim.y + 2*const_khalf);
+        const int size = (blockDim.x + 2*const_khalf)*(blockDim.y + 2*const_khalf);
 
         __shared__ pixel_t subMatrix[size];
 
