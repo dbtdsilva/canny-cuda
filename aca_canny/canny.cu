b1f338257e728313d8eda78061c01841bc04a395
Eduardo Sousa <eduardosousa@ua.pt>
2015-01-13 13:43:50 +0000

    Shared memory - fix 016

--- aca_canny/canny.cu
+++ aca_canny/canny.cu
@@ -295,11 +295,11 @@ __global__  void convolution_kernel(const pixel_t *in, const float *kernel, pixe
 
         if(sub_x == 1 && sub_y == 1)
             subMatrix[(sub_y-1)*width + sub_x-1] = in[(y-1)*const_nx + x-1];
-        else if(sub_x == 1 && vLimit)
+        else if(sub_x == 1 && (vLimit || sub_y == height-1))
             subMatrix[(sub_y+1)*width + sub_x-1] = in[(y+1)*const_nx + x-1];
-        else if(hLimit && sub_y == 1)
+        else if((hLimit || sub_x == width-1) && sub_y == 1)
             subMatrix[(sub_y-1)*width + sub_x+1] = in[(y-1)*const_nx + x+1];
-        else if(hLimit && vLimit)
+        else if((hLimit || sub_x == width-1) && (vLimit || sub_y == height-1))
             subMatrix[(sub_y+1)*width + sub_x+1] = in[(y+1)*const_nx + x+1];
 
         if(sub_x == 1)
